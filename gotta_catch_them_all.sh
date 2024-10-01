a34406ab90116ff46a38fae8acf365172cb8bce1
TCHEGNIMONHAN David <tchegnimonhan-david@tchegnimonhan-david-HP-Pavilion-Laptop-14-dv2xxx.org>
2024-10-01 18:41:08 +0100

    envoyer

---
+++ gotta_catch_them_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+echo "ffiche le nombre de personnes dont le nom de famille commence par la chaîne donnée en argument."
+
+
+echo "Nombre déargument:10" $#
+
+
+echo "ARG0 ARG1 ARG2 ARG3 ARG4 ARG5 ARG6 ARG7 ARG8 ARG9" $@
+
+
+
