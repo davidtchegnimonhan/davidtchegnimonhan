8f77aa3f466b74cca581191633dc209a11363927
TCHEGNIMONHAN David <tchegnimonhan-david@tchegnimonhan-david-HP-Pavilion-Laptop-14-dv2xxx.org>
2024-10-04 12:10:27 +0100

    send

---
+++ my_swap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void my_swap (int *a, int *b);
+{
+  int temp = 0;//B
+
+     temp =*b
+     *b =*a
+     *a = temp
+}
+
+void my_putchar (char)
+int main(void);
+{
+  int a = 15;
+  int b = 16;
+  
+  my_putchar("a = %p\n", a);
+  my_putchar("a = %p\n", b);
+
+    Return 0;
+}    
