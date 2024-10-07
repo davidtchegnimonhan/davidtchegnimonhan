95016c968812417c9fc1885c28a006d2450a2e76
TCHEGNIMONHAN David <tchegnimonhan-david@tchegnimonhan-david-HP-Pavilion-Laptop-14-dv2xxx.org>
2024-10-07 12:58:24 +0100

    envoie

---
+++ my_compute_factoriel_rec.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** my_compute_factoriel_rec.c
+** File description:
+** what they ask
+*/
+
+int my_compute_factorial_rec(int nb)
+{
+    int index;
+    int res;
+
+    index = nb;
