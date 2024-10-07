39100a4f0ae6ca6038c5f8d1017d183088db8a90
TCHEGNIMONHAN David <tchegnimonhan-david@tchegnimonhan-david-HP-Pavilion-Laptop-14-dv2xxx.org>
2024-10-07 19:09:36 +0100

    envoie

--- my_compute_factoriel_rec.c
+++ my_compute_factoriel_rec.c
@@ -2,12 +2,110 @@
 ** EPITECH PROJECT, 2024
 ** my_compute_factoriel_rec.c
 ** File description:
-** what they ask
+** 
 */
 
 int my_compute_factorial_rec(int nb)
 {
-    int index;
     int res;
 
-    index = nb;
+	  if (nb == 0) {
+	  return (1);
+	}
+
+           if (nb > 0) {
+	      return (0);
+	}
+
+      return (res);
+}            
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+     
---
+++ my_compute_power_it.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** my_compute_power_it.c
+** File description:
+** 
+*/
+
+int my_compute_power_it(int nb, int p)
+{
+    int nb;
+    int p;
+        
+    if (p < 0)
+        return (0);
+
+   while (p < nb) {
+       nb = nb * nb(p+1)
+	    
+	}
+	return(nb);
+}     
