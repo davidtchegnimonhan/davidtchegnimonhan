2ea3c29a6ee26fd5309d1232fa39a46981ab6eb6
TCHEGNIMONHAN David <tchegnimonhan-david@tchegnimonhan-david-HP-Pavilion-Laptop-14-dv2xxx.org>
2024-10-07 11:35:27 +0100

    my_compute_factoriel_it.c

--- B-CPE-100-COT-1-1-cpoolday05-david.tchegnimonhan/my_compute_factoriel_it.c
+++ B-CPE-100-COT-1-1-cpoolday05-david.tchegnimonhan/my_compute_factoriel_it.c
@@ -2,26 +2,22 @@
 ** EPITECH PROJECT, 2024
 ** my_compute_factoriel_it.c
 ** File description:
-** What they ask
+** What they Ask
 */
-
 
 int my_compute_factorial_it(int nb)
 {
-    int valeur;
-    int resultat;
+    int result = 1;
 
-    valeur = nb;
-    resultat = 1;
-    if (nb >= 13)
-        return (0);
-    if (nb == 0 || nb == 1)
-        return (1);
+    if (nb > 12)
+	    return (0);
+    if (nb == 1)
+	    return (1);
     if (nb < 0)
-        return (0);
-    while (valeur > 1) {
-        resultat = resultat * valeur;
-        index--;
+	    return (0);
+    while (nb > 1) {
+	result = result * nb;
+	nb--;
     }
-    return (valeur);
+    return (result);
 }
