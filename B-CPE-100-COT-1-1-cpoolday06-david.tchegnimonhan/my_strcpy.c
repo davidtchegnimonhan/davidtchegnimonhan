571c4d4311970489c8b990233555330f970e6d9e
TCHEGNIMONHAN David <tchegnimonhan-david@tchegnimonhan-david-HP-Pavilion-Laptop-14-dv2xxx.org>
2024-10-08 10:17:05 +0100

    my_strcpy.committed

---
+++ B-CPE-100-COT-1-1-cpoolday06-david.tchegnimonhan/my_strcpy.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** my_strcpy.c
+** File description:
+** Task 01 - my_strcpy/c
+*/
+
+#include <stdio.h> 
+#include <string.h>
+
+
+
+char *my_strcpy(char *dest, char const *src)
+{
+  my_putchar(*dest = src);
+                 
+     while(src !=\'0'){
+           dest++;
+           src++;
+     }
+
+     return (dest)
+}
+
