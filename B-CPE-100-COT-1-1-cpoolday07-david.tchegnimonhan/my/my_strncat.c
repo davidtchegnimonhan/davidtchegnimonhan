4d4d18943ddf6d77174d27bd9d104b4fadc5b181
TCHEGNIMONHAN David <tchegnimonhan-david@tchegnimonhan-david-HP-Pavilion-Laptop-14-dv2xxx.org>
2024-10-18 15:55:16 +0100

    my_put_nbr.c updated

---
+++ B-CPE-100-COT-1-1-cpoolday07-david.tchegnimonhan/my/my_strncat.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** my_strncat.c
+** File description:
+** 
+*/
+
+#include <stdio.h>
+
+char *my_strncat(char *dest, char const *src, int nb) {
+
+    char *dest[];
+    
+       char const *src = 
+
+
+
+
+           
+       return(0);
+}
