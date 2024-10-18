821590daacd13e857a33223349ca2812c2d71cd3
TCHEGNIMONHAN David <tchegnimonhan-david@tchegnimonhan-david-HP-Pavilion-Laptop-14-dv2xxx.org>
2024-10-18 16:27:21 +0100

    my_strcmp.c updated

---
+++ B-CPE-100-COT-1-1-cpoolday07-david.tchegnimonhan/my/my_strcmp.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** my_strcmp.c
+** File description:
+** 
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int my_strcmp(char *strg1, char *strg2)
+{
+    while( ( *strg1 != '\0' && *strg2 != '\0' ) && *strg1 == *strg2 )
+    {
+        strg1++;
+        strg2++;
+    }
+
+    if(*strg1 == *strg2)
+    {
+        return 0; // strings are identical
+    }
+
+    else
+    {
+        return *strg1 - *strg2;
+    }
+}
