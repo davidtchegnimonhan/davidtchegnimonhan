1d48511aff44cdc16e73841609aa6f464aa3b69e
TCHEGNIMONHAN David <tchegnimonhan-david@tchegnimonhan-david-HP-Pavilion-Laptop-14-dv2xxx.org>
2024-10-18 12:48:02 +0100

    my_strcat.c updated

---
+++ B-CPE-100-COT-1-1-cpoolday07-david.tchegnimonhan/my/my_strcat.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** my_strcat.c
+** File description:
+** 
+*/
+
+#include <unstid.h> 
+
+void my_putchar(char c) {
+    write(1, &c, 1);
+}
+
+char *my_strcat(char *dest, char const *src);
+{
+    char *dest[] = {'d', 'a', 'v', 'i', 'd'};
+    write(1, 'david\n', 6);
+    
+    char const *src[] = {'g', 'o', 'a', 't'};
+    write(1, 'goat\n', 5);
+    
+    return(0)
+}
