48d0cc7dc77df75908fdd125e13eae4cc75839a3
TCHEGNIMONHAN David <tchegnimonhan-david@tchegnimonhan-david-HP-Pavilion-Laptop-14-dv2xxx.org>
2024-10-19 09:26:43 +0100

    my_strcmp.c updated

--- B-CPE-100-COT-1-1-cpoolday07-david.tchegnimonhan/my/my_strcmp.c
+++ B-CPE-100-COT-1-1-cpoolday07-david.tchegnimonhan/my/my_strcmp.c
@@ -10,7 +10,7 @@
 
 int my_strcmp(char *strg1, char *strg2)
 {
-    while( ( *strg1 != '\0' && *strg2 != '\0' ) && *strg1 == *strg2 )
+    while((*strg1 != '\0' && *strg2 != '\0') && *strg1 == *strg2)
     {
         strg1++;
         strg2++;
@@ -18,7 +18,7 @@ int my_strcmp(char *strg1, char *strg2)
 
     if(*strg1 == *strg2)
     {
-        return 0; // strings are identical
+        return 0; 
     }
 
     else
