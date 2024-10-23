19bb62733f42f67f1c97d83a4d657a017e48c865
TCHEGNIMONHAN David <tchegnimonhan-david@tchegnimonhan-david-HP-Pavilion-Laptop-14-dv2xxx.org>
2024-10-23 09:08:21 +0100

    my_compute_factoriel_it.c committed

--- B-CPE-100-COT-1-1-cpoolday05-david.tchegnimonhan/my_compute_factoriel_it.c
+++ B-CPE-100-COT-1-1-cpoolday05-david.tchegnimonhan/my_compute_factoriel_it.c
@@ -2,22 +2,25 @@
 ** EPITECH PROJECT, 2024
 ** my_compute_factoriel_it.c
 ** File description:
-** What they Ask
+** what they Ask
 */
 
 int my_compute_factorial_it(int nb)
 {
-    int result = 1;
-
-    if (nb > 12)
-	    return (0);
-    if (nb == 1)
-	    return (1);
-    if (nb < 0)
-	    return (0);
+   int result = 1;
+   int nb
+   if (nb > 12) {
+       return (0);
+   }
+   if (nb == 1) {
+       return (1);
+   }
+   if (nb < 0) {
+       return (0);
+   }
     while (nb > 1) {
 	result = result * nb;
 	nb--;
-    }
-    return (result);
+   }
+   return (result);
 }
